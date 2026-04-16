27b65a42a28f0104d7ca8789fce6fa34937dcbd0
timeturtles <bezhu@ucsd.edu>
2026-04-15 18:15:05 -0700

    added current class functionality to js file

--- global.js
+++ global.js
@@ -3,3 +3,14 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+if (currentLink) {
+  currentLink.classList.add('current');
+}
+
