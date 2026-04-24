dc7bbec2232d2bf2139f93ebfc9c2e8ddab82d94
timeturtles <bezhu@ucsd.edu>
2026-04-24 00:17:50 -0700

    Update global.js

--- global.js
+++ global.js
@@ -76,7 +76,7 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
   containerElement.innerHTML = '';
   const article = document.createElement('article');
   article.innerHTML = `
-    <h3>${project.title}</h3>
+    <headingLevel>${project.title}</headingLevel>
     <img src="${project.image}" alt="${project.title}">
     <p>${project.description}</p>
   `;
