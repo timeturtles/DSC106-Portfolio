77cfde3c1ad6d69d765aacbf5f1081ec1e18b309
timeturtles <bezhu@ucsd.edu>
2026-04-24 02:03:33 -0700

    incorporated github stats into the main page

--- global.js
+++ global.js
@@ -84,3 +84,7 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
     containerElement.appendChild(article);
   }
 }
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
